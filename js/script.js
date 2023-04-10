1c74144e90166ebbcd78155ff2599470c79637c0
luizgnclvs <luizgnclvss@gmail.com>
2023-04-10 15:18:34 -0300

    Impl. requisição de palavra

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
-	
+const baseURL = 'https://api.dicionario-aberto.net';
+
+const newGameButton = document.querySelector('.new-game');
+const resetButton = document.querySelector('.reset');
+
+const bgColorInput = document.getElementById('bg-color');
+const boardColorInput = document.getElementById('board-color');
+
+const wordElement = document.querySelector('.word');
+
+let word;
+let hint;
+let characterElements = [];
+
+let gameStart = false;
+let mistakes = 0;
+
+let canvasContext;
+
+async function requestWord () {
+	const response = await fetch(`${baseURL}/random`);
+
+	if (response.ok) {
+		const result = await response.json();
+		const description = await requestDescription(result.word);
+
+		if (description && isDescriptionValid(description)) {
+			word = result.word;
+			hint = description;	
+		} else requestWord();
+	}
+}
+
+async function requestDescription (word) {
+	const response = await fetch(`${baseURL}/word/${word}/1`)
+
+	if (response.ok) {
+		const result = await response.json();
+		return treatDescription(result[0].xml);
+	} else return null;
+}
+
+requestWord()
+
+function treatDescription (description) {
+	return description
+		.split('def>')[1]
+		.split('.')[0]
+		.split(',')[0]
+		.split(';')[0]
+		.split(':')[0]
+		.split('^')[0]
+		.replace(/\n/g, ' ')
+		.replace(/_/g, '')
+		.replace(/\[/g, '')
+		.replace(/]/g, '')
+		.toLowerCase()
+		.trim();
+}
+
+function isDescriptionValid (description) {
+	return description.length >= 6 && description.length <= 30;
+}
